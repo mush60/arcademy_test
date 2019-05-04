8987057c876a194cacdefb1af10100053d3f1771
mush60 <mushti8@gmail.com>
2019-05-04 13:06:09 +0800

    add komentar

--- soal2.php
+++ soal2.php
@@ -2,10 +2,10 @@
 
 function ValidasiForm($usrnm, $pass) {
 
-    if ($usrnm != null && $pass != null) {
-        if (strlen($usrnm)>=8 && strlen($pass)>=8) {
+    if ($usrnm != null && $pass != null) { //check apakah inputan null 
+        if (strlen($usrnm)>=8 && strlen($pass)>=8) { // check apakah panjang inputan tidak lebih kecil dari 8
             if (!preg_match('@[A-Z]@', $pass) || !preg_match('@[a-z]@', $pass) ||
-            !preg_match('@[0-9]@', $pass) || !preg_match('@[^\w]@', $pass)) {
+            !preg_match('@[0-9]@', $pass) || !preg_match('@[^\w]@', $pass)) { //regex untung mengeck apakah terdapat huruf besar, kecil, angka,dan spesial karakter
                 echo "Fail Weak Password\n";
             }
             else {
@@ -25,11 +25,11 @@ function ValidasiForm($usrnm, $pass) {
 
 }
 
-echo "input username : ";
+echo "input username : ";           //get input from user
 $username = trim(fgets(STDIN));
 
-echo "password : ";
+echo "password : ";                 //get input from user
 $password = trim(fgets(STDIN));
 
 
-ValidasiForm($username,$password);
+ValidasiForm($username,$password);      //memanggil fungsi validasi dengap paremeter inputan user
