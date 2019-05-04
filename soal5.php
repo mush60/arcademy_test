119a62cd3ab260270e04c380bafb86c5cd047fae
mush60 <mushti8@gmail.com>
2019-05-04 20:32:43 +0800

    revisi soal 5 string 32 caracter yang tidak boleh sama

--- soal5.php
+++ soal5.php
@@ -1,10 +1,21 @@
 <?php
+
 function PrintStr($jml) {
+
     for($i=0; $i<$jml; $i++) {
-        $strarr[$i] = md5(rand(1,1000));
+
+        $strarr[$i] = md5($i);
+        //agar tidak ada string sama maka 
+        //tiap character yang dihash harus berbeda
+
     }
+
     print_r($strarr);
+
 }
-echo "input jumlah output : ";           //get input from user
+
+echo "input jumlah output : ";
 $jmlstr = trim(fgets(STDIN));
+
+
 PrintStr($jmlstr);
